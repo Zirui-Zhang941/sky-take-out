4c753090f0c2a227ff84f9d735d11e8a36e2c412
Violet <2536113502@qq.com>
2024-09-10 10:41:12 -0500

    employee edit feature done

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -1,9 +1,9 @@
 package com.sky.mapper;
 
+import com.github.pagehelper.Page;
+import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
 
 @Mapper
 public interface EmployeeMapper {
@@ -20,4 +20,15 @@ public interface EmployeeMapper {
             "VALUES ( #{name}, #{username}, #{password}, #{phone}, #{sex}, #{idNumber}, #{status}, #{createTime}, #{updateTime}, #{createUser}, #{updateUser})")
     void insert(Employee employee);
 
+    /**
+     * 分页查询mapper层
+     * @param employeePageQueryDTO
+     * @return
+     */
+    Page<Employee> pageQuery(EmployeePageQueryDTO employeePageQueryDTO);
+
+    void changestatus(Employee employee);
+
+    @Select("select * from employee where id = #{id}")
+    Employee searchbyid(Long id);
 }
--- sky-server/src/main/java/com/sky/service/EmployeeService.java
+++ sky-server/src/main/java/com/sky/service/EmployeeService.java
@@ -2,7 +2,9 @@ package com.sky.service;
 
 import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
+import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
+import com.sky.result.PageResult;
 
 public interface EmployeeService {
 
@@ -19,4 +21,29 @@ public interface EmployeeService {
      */
     void addemployee(EmployeeDTO employeeDTO);
 
+    /**
+     * 员工分页查询
+     * @param employeePageQueryDTO
+     * @return
+     */
+    PageResult pageQuery(EmployeePageQueryDTO employeePageQueryDTO);
+
+    /**
+     * 禁用员工
+     * @param status
+     * @param id
+     */
+    void changestatus(Integer status, Long id);
+
+    /**
+     * 员工id查询
+     * @param id
+     * @return
+     */
+    Employee searchbyId(Long id);
+
+    /**
+     * 更新员工信息
+     */
+    void updateinfo(EmployeeDTO employeeDTO);
 }
